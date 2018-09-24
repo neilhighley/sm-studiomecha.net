d61fb3cac6a9ca543fe599bd734a9e9ee20eebde
Neil Highley <neil@neilhighley.com>
2018-09-24 08:49:25 +0100

    Adding aframe example

--- studiomecha/src/VRAFrame.js
+++ studiomecha/src/VRAFrame.js
@@ -1,14 +1,18 @@
-import React,{Component} from 'react'
-import {Link} from 'react-router-dom'
-
+import 'aframe';
+import 'aframe-particle-system-component';
+import {Entity, Scene} from 'aframe-react';
+import React,{Component} from 'react';
 
 class VRAFrame extends Component{
     render(){
         return(
-            <div>
-                <p>A Frame Example</p>
-                <Link to='/home'>Back Home</Link>
-            </div>
+            <Scene>
+                <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+                <Entity particle-system={{preset: 'snow'}}/>
+                <Entity light={{type: 'point'}}/>
+                <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
+                <Entity text={{value: 'Hello, WebVR!'}}/>
+            </Scene>
         )
     }
 }
--- studiomecha/src/VRCube.js
+++ studiomecha/src/VRCube.js
@@ -4,7 +4,11 @@ import {Link} from 'react-router-dom'
 class VRCube extends Component{
     render(){
         return(
-            <Link to='./virtual/aframe'><img src="./images/vr_1.png" alt="virtual reality example"/></Link>
+            <div>
+                <Link to='./virtual/aframe'><img src="./images/vr_1.png" alt="virtual reality example"/></Link>
+                <a href='VRAFrame-standalone.html'>VR Example (New Window)</a>
+                <Link to='./virtual/aframe'>VR Example (This Window)</Link>
+            </div>
         )
     }
 }
